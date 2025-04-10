4d569ca8bfc2a0b99c2bff43bbcb2ff012b90382
Gilnathan <gilnathan.dev@icloud.com>
2025-04-10 20:44:00 -0300

    add filers

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,10 @@
-import styles from "./Main.module.css"
-import Countdown from "@/components/Countdown/Countdown"
-import Button from "../Button/Button"
+import styles from "./Main.module.css";
+import Countdown from "@/components/Countdown/Countdown";
+import Button from "../Button/Button";
 
 export default function Main() {
   return (
     <div className={styles.mainContainer}>
-      
       <video
         className={styles.bgVideo}
         src="/videoBG.mp4"
@@ -14,40 +13,33 @@ export default function Main() {
         muted
         playsInline
       />
-    
 
       <div className={styles.containerInf}>
-            <h1>
-                <span className={styles.titleOrange}>Porto Seguro</span> <br />
-                <span className={styles.titleWhite}>Summer Geek</span>
-             </h1>
-            
-            <p className={styles.subtitle}>
-            "Um encontro de almas que compartilham a paixão pela fantasia"
-            </p>
+        <h1>
+          <span className={styles.titleOrange}>Porto Seguro</span> <br />
+          <span className={styles.titleWhite}>Summer Geek</span>
+        </h1>
 
-            <div className={styles.infoContainer}>
+        <p className={styles.subtitle}>
+          "Um encontro de almas que compartilham a paixão pela fantasia"
+        </p>
 
-                <div className={styles.eventInfo}>
-                    <span className={styles.label}>Data do Evento</span>
-                    <strong>12/10/2025</strong>
-                </div>
+        <div className={styles.infoContainer}>
+          <div className={styles.eventInfo}>
+            <span className={styles.label}>Data do Evento</span>
+            <strong>12/10/2025</strong>
+          </div>
 
-            <div className={styles.eventInfo}>
-                <span className={styles.label}>Local</span>
-                <strong>Porto Seguro, BA</strong>
-            </div>
-            
-            </div>
-
-           
-            <Button />
-            
-
+          <div className={styles.eventInfo}>
+            <span className={styles.label}>Local</span>
+            <strong>Porto Seguro, BA</strong>
+          </div>
         </div>
-        
+
+        <Button />
+      </div>
+
       <Countdown />
-      
     </div>
-  )
+  );
 }
